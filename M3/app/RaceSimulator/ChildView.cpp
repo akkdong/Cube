3e65ec372d17dc98ff13ea399183a53df1240123
Kangwoo Cheon <kwcheon@astams.com>
2023-11-21 07:45:52 +0900

    another optimize method: test

--- M3/app/RaceSimulator/ChildView.cpp
+++ M3/app/RaceSimulator/ChildView.cpp
@@ -77,7 +77,7 @@ int CChildView::OnCreate(LPCREATESTRUCT lpCreateStruct)
 
 	m_wndOpen.Create(_T("Open"), WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON, CRect(CPoint(10, 10), CSize(120, 32)), this, IDC_OPEN);
 	m_wndZoomIn.Create(_T("Zoom In"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, CRect(CPoint(10, 50), CSize(120, 32)), this, IDC_ZOOMIN);
-	m_wndZoomOut.Create(_T("Zoom OUt"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, CRect(CPoint(10, 90), CSize(120, 32)), this, IDC_ZOOMOUT);
+	m_wndZoomOut.Create(_T("Zoom Out"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, CRect(CPoint(10, 90), CSize(120, 32)), this, IDC_ZOOMOUT);
 	m_wndNext.Create(_T("Next"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, CRect(CPoint(10, 130), CSize(120, 32)), this, IDC_NEXT);
 	m_wndPrev.Create(_T("Prev"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, CRect(CPoint(10, 170), CSize(120, 32)), this, IDC_PREV);
 
@@ -162,7 +162,129 @@ void CChildView::OnPaint()
 		DrawPilot(&dc, cx, cy, mPilotPosPtr->getLatitude(), mPilotPosPtr->getLongitude());
 	}
 
+	if (mTask.getTurnPointCount() > 1 && mStartTurnPoint + 1 < mTask.getTurnPointCount())
+	{
+		const GeographicLib::Geodesic &geod = GeographicLib::Geodesic::WGS84();
+
+		XcTurnPointPtr pt1 = mTask.getTurnPoint(mTask.getTurnPointCount() - 1);
+		Math::real prevLat = pt1->getLatitude();
+		Math::real prevLon = pt1->getLongitude();
+		XcTurnPointPtr pt2, pt3;
+		Math::real lat2, lon2, lat3, lon3;
+		Math::real total = 0 - pt1->getRadius();
+
+		CPen pen(PS_SOLID, 1, RGB(0, 0, 255));
+		CPen *pOldPen = dc.SelectObject(&pen);
+
+		if (mStartTurnPoint + 2 == mTask.getTurnPointCount())
+		{
+			ASSERT(mPilotPosPtr);
+			lat3 = mPilotPosPtr->getLatitude();
+			lon3 = mPilotPosPtr->getLongitude();
+		}
+		else
+		{
+			for (size_t i = mTask.getTurnPointCount() - 1; i >= mStartTurnPoint + 2; --i)
+			{
+				// pt1: i - 0, pt2: i - 1, pt3: i - 2
+				//
+				pt2 = mTask.getTurnPoint(i - 1);
+				pt3 = mTask.getTurnPoint(i - 2);
+				lat2 = pt2->getLatitude();
+				lon2 = pt2->getLongitude();
+				lat3 = pt3->getLatitude();
+				lon3 = pt3->getLongitude();
+
+				if ((i - 2) == mStartTurnPoint && mPilotPosPtr)
+				{
+					lat3 = mPilotPosPtr->getLatitude();
+					lon3 = mPilotPosPtr->getLongitude();
+				}
+
+				// exception1: line(1, 3) pass through inside cylinder(2)
+				// exception2: more than 2 points have same position
+
+				Math::real dist, azi1, azi2;
+				Math::real lat, lon;
+
+				geod.Inverse(prevLat, prevLon, lat3, lon3, dist, azi1, azi2);
+				geod.Direct(prevLat, prevLon, azi1, dist / 2, lat, lon);
+
+				geod.Inverse(lat2, lon2, lat, lon, dist, azi1, azi2);
+				geod.Direct(lat2, lon2, azi1, pt2->getRadius(), lat, lon);
+
+				DrawLine(&dc, cx, cy, prevLat, prevLon, lat, lon);
+				geod.Inverse(prevLat, prevLon, lat, lon, dist);
+				total += dist;
+				//TRACE("draw from %d to %d\n", i, i - 1);
+
+				prevLat = lat;
+				prevLon = lon;
+			}
+		}
+
+		DrawLine(&dc, cx, cy, prevLat, prevLon, lat3, lon3);	
+		Math::real dist;
+		geod.Inverse(prevLat, prevLon, lat3, lon3, dist);
+		total += dist;
+		TRACE("Total distance : %.1f\n", total / 1000.0);
+
+		dc.SelectObject(pOldPen);
+	}
+
+	/*
+	if (mStartTurnPoint + 2 < mTask.getTurnPointCount())
+	{
+		// point #1, #2, #3
+		auto pt1 = mTask.getTurnPoint(mStartTurnPoint + 0);
+		auto pt2 = mTask.getTurnPoint(mStartTurnPoint + 1);
+		auto pt3 = mTask.getTurnPoint(mStartTurnPoint + 2);
+
+		Math::real lat1 = pt1->getLatitude();
+		Math::real lon1 = pt1->getLongitude();
+		Math::real lat2 = pt2->getLatitude();
+		Math::real lon2 = pt2->getLongitude();
+		Math::real lat3 = pt3->getLatitude();
+		Math::real lon3 = pt3->getLongitude();
+
+		if (mPilotPosPtr)
+		{
+			lat1 = mPilotPosPtr->getLatitude();
+			lon1 = mPilotPosPtr->getLongitude();
+		}
+
+		CPen pen(PS_SOLID, 2, RGB(0, 0, 255));
+		CPen *pOldPen = dc.SelectObject(&pen);
+
+		// draw triangle
+		DrawLine(&dc, cx, cy, lat1, lon1, lat2, lon2);
+		DrawLine(&dc, cx, cy, lat1, lon1, lat3, lon3);
+		DrawLine(&dc, cx, cy, lat3, lon3, lat2, lon2);
+
+		// line #1 --> mid(#2, #3)
+		DrawIntersectLine(&dc, cx, cy, lat1, lon1, lat2, lon2, lat3, lon3);
+		// line #2 --> mid(#1, #3)
+		DrawIntersectLine(&dc, cx, cy, lat2, lon2, lat1, lon1, lat3, lon3);
+		// line #3 --> mid(#1, #2)
+		DrawIntersectLine(&dc, cx, cy, lat3, lon3, lat1, lon1, lat2, lon2);
+
+		dc.SelectObject(pOldPen);
+	}
+	*/
+
 	dc.RestoreDC(nSaveDC);
+}
+
+void CChildView::DrawIntersectLine(CDC *pDC, int cx, int cy, Math::real lat1, Math::real lon1, Math::real lat2, Math::real lon2, Math::real lat3, Math::real lon3)
+{
+	const GeographicLib::Geodesic &geod = GeographicLib::Geodesic::WGS84();
+	Math::real dist, azi1, azi2;
+	Math::real lat, lon;
+
+	geod.Inverse(lat2, lon2, lat3, lon3, dist, azi1, azi2);
+	geod.Direct(lat2, lon2, azi1, dist / 2, lat, lon);
+
+	DrawLine(pDC, cx, cy, lat1, lon1, lat, lon);
 }
 
 void CChildView::DrawCircle(CDC* pDC, int cx, int cy, Math::real lat, Math::real lon, Math::real radius)
@@ -327,8 +449,8 @@ void CChildView::OnFileOpen()
 			FindBoundary(&mTask, mBoundary);
 #endif
 
-			mCenterPos.setLatitude(mBoundary[GEO_S] + (mBoundary[GEO_N] - mBoundary[GEO_S]) / 2.0);
-			mCenterPos.setLongitude(mBoundary[GEO_W] + (mBoundary[GEO_E] - mBoundary[GEO_W]) / 2.0);
+			mCenterPos.setLatitude(mBoundary[GEO_S] + (mBoundary[GEO_N] - mBoundary[GEO_S]) / 2.0f);
+			mCenterPos.setLongitude(mBoundary[GEO_W] + (mBoundary[GEO_E] - mBoundary[GEO_W]) / 2.0f);
 
 			// calcualte zoom-factor
 			CRect rect;
@@ -395,13 +517,13 @@ void CChildView::FindBoundary(XcTask *task, Math::real *border)
 
 void CChildView::OnZoomIn()
 {
-	mZoomRatio += mZoomRatio * 0.1;
+	mZoomRatio += mZoomRatio * 0.1f;
 	Invalidate();
 }
 
 void CChildView::OnZoomOut()
 {
-	mZoomRatio -= mZoomRatio * 0.1;
+	mZoomRatio -= mZoomRatio * 0.1f;
 	Invalidate();
 }
 
@@ -495,8 +617,8 @@ void CChildView::OnLButtonDblClk(UINT nFlags, CPoint point)
 		Math::real angle = std::atan2<Math::real>(dx, -dy);
 
 		// radian to degree
-		const Math::real pi = 3.14159265359;
-		angle = angle * 180.0 / pi;
+		const Math::real pi = 3.14159265359f;
+		angle = angle * 180.0f / pi;
 		if (angle < 0)
 			angle = 360 + angle;
 		// screen to geo
@@ -633,3 +755,106 @@ void CChildView::Optimize(size_t startPoint, Math::real lat, Math::real lon)
 	TRACE("OPTIMIZE repeat-count : %d\n", mTask.repeatCount);
 #endif
 }
+
+
+
+//
+//
+//
+
+#include <iostream>
+#include <iomanip>
+#include <GeographicLib/Gnomonic.hpp>
+#include <GeographicLib/Geodesic.hpp>
+
+class vector3 {
+public:
+	Math::real _x, _y, _z;
+	vector3(Math::real x, Math::real y, Math::real z = 1) throw()
+		: _x(x)
+		, _y(y)
+		, _z(z) {}
+	vector3 cross(const vector3& b) const throw() {
+		return vector3(_y * b._z - _z * b._y,
+			_z * b._x - _x * b._z,
+			_x * b._y - _y * b._x);
+	}
+	void norm() throw() {
+		_x /= _z;
+		_y /= _z;
+		_z = 1;
+	}
+};
+
+class Test
+{
+public:
+	Test() {
+		main();
+	}
+
+	void main() {
+		Math::real lata1, lona1, lata2, lona2;
+		Math::real latb1, lonb1, latb2, lonb2;
+		//std::cin >> lata1 >> lona1 >> lata2 >> lona2
+		//	>> latb1 >> lonb1 >> latb2 >> lonb2;
+		lata1 = 37.4858f;
+		lona1 = 126.5355f;
+		lata2 = 35.4532f;
+		lona2 = 129.2097f;
+		latb1 = 36.3656f;
+		lonb1 = 130.929f;
+		latb2 = 35.5598f;
+		lonb2 = 126.8237f;
+
+		const GeographicLib::Geodesic
+			geod(GeographicLib::Constants::WGS84_a(),
+				GeographicLib::Constants::WGS84_f());
+		const GeographicLib::Gnomonic gn(geod);
+		Math::real
+			lat0 = (lata1 + lata2 + latb1 + latb2) / 4,
+			// Possibly need to deal with longitudes wrapping around
+			lon0 = (lona1 + lona2 + lonb1 + lonb2) / 4;
+		//std::cout << std::setprecision(16);
+		//std::cout << "Initial guess " << lat0 << " " << lon0 << "\n";
+		TRACE("Initial guess : %f %f\n", lat0, lon0);
+		for (int i = 0; i < 10; ++i) {
+			Math::real xa1, ya1, xa2, ya2;
+			Math::real xb1, yb1, xb2, yb2;
+			gn.Forward(lat0, lon0, lata1, lona1, xa1, ya1);
+			gn.Forward(lat0, lon0, lata2, lona2, xa2, ya2);
+			gn.Forward(lat0, lon0, latb1, lonb1, xb1, yb1);
+			gn.Forward(lat0, lon0, latb2, lonb2, xb2, yb2);
+			// See Hartley and Zisserman, Multiple View Geometry, Sec. 2.2.1
+			vector3 va1(xa1, ya1); vector3 va2(xa2, ya2);
+			vector3 vb1(xb1, yb1); vector3 vb2(xb2, yb2);
+			// la is homogeneous representation of line A1,A2
+			// lb is homogeneous representation of line B1,B2
+			vector3 la = va1.cross(va2);
+			vector3 lb = vb1.cross(vb2);
+			// p0 is homogeneous representation of intersection of la and lb
+			vector3 p0 = la.cross(lb);
+			p0.norm();
+			Math::real lat1, lon1;
+			gn.Reverse(lat0, lon0, p0._x, p0._y, lat1, lon1);
+			//std::cout << "Increment " << lat1 - lat0 << " " << lon1 - lon0 << "\n";
+			TRACE("Increment %f %f\n", lat1 - lat0, lon1 - lon0);
+			lat0 = lat1;
+			lon0 = lon1;
+		}
+		//std::cout << "Final result " << lat0 << " " << lon0 << "\n";
+		TRACE("Final result %f %f\n", lat0, lon0);
+		Math::real azi0, azi1, azi2;
+		geod.Inverse(lata1, lona1, lat0, lon0, azi0, azi2);
+		//std::cout << "Azimuths on line A " << azi2 << " ";
+		geod.Inverse(lat0, lon0, lata2, lona2, azi1, azi0);
+		//std::cout << azi1 << "\n";
+		TRACE("Azimuths on line A %f %f\n", azi2, azi1);
+		geod.Inverse(latb1, lonb1, lat0, lon0, azi0, azi2);
+		//std::cout << "Azimuths on line B " << azi2 << " ";
+		geod.Inverse(lat0, lon0, latb2, lonb2, azi1, azi0);
+		//std::cout << azi1 << "\n";
+		TRACE("Azimuths on line B %f %f\n", azi2, azi1);
+	}
+
+} _t;
--- M3/app/RaceSimulator/ChildView.h
+++ M3/app/RaceSimulator/ChildView.h
@@ -80,6 +80,8 @@ public:
 	void DrawLine(CDC* pDC, int cx, int cy, Math::real lat1, Math::real lon1, Math::real lat2, Math::real lon2);
 	void DrawPilot(CDC* pDC, int cx, int cy, Math::real lat, Math::real lon);
 
+	void DrawIntersectLine(CDC *pDC, int cx, int cy, Math::real lat1, Math::real lon1, Math::real lat2, Math::real lon2, Math::real lat3, Math::real lon3);
+
 	void FindBoundary(XcTask *task, Math::real *border);
 	void Optimize(size_t startPoint, Math::real lat, Math::real lon);
 
